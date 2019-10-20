486bb1a2b014fb43cd0dc7fad568fdc0227c9943
SamsonAudrey <32452495+SamsonAudrey@users.noreply.github.com>
2019-10-20 19:12:55 +0200

    Update blobTest.scala

--- src/test/scala/toolsTest/blobTest.scala
+++ src/test/scala/toolsTest/blobTest.scala
@@ -18,14 +18,14 @@ class blobTest extends FunSpec with Matchers with GivenWhenThen with BeforeAndAf
   describe("If you create a blob") {
     describe("with empty String params") {
       it("it should create it ") {
-        val blob = Blob("","","")
+        val blob = Blob.apply("","","")
         assert(blob.hash === "" && blob.filePath === "")
       }
     }
 
     describe("with all arguments") {
       it("it should create it ") {
-        val blob2 = Blob("a","b","c")
+        val blob2 = Blob.apply("a","b","c")
         assert(blob2.hash === "b" && blob2.filePath === "a")
       }
     }
